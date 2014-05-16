f641e35dfe6c5d79f371c6d33c9614336576e357
Ilya Antipenko <aivus@aivus.name>
2014-05-16 17:21:19 +0300

    Fadeout for remove all tasks action

--- web/js/todo.js
+++ web/js/todo.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
 
     $('#dropTasks').click(function(){
         makeApiRequest('DELETE', null, function(){
-            $('#tasksArea').empty();
+            $('#tasksArea').fadeOut();
         });
     });
 
