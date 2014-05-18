6d8d5a0f8ba11599c256c6c78ee7ebd3178d56dd
Ilya Antipenko <aivus@aivus.name>
2014-05-18 19:06:42 +0300

    Remove excess behavior

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -7,18 +7,6 @@ use yii\web\Controller;
 use yii\filters\VerbFilter;
 class SiteController extends Controller
 {
-    public function behaviors()
-    {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
-
     public function actions()
     {
         return [
